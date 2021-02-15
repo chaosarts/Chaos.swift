8f9884dae760f36204fc7a15de06ab905c2fcb4c
Fu Lam Diep <fdiep@initse.com>
2021-02-15 09:29:17 +0100

    UPDATE Added description to static floats for angles

--- ChaosGraphics/Classes/Extensions/CGFloat+Angle.swift
+++ ChaosGraphics/Classes/Extensions/CGFloat+Angle.swift
@@ -8,10 +8,23 @@
 import CoreGraphics
 
 public extension CGFloat {
+
+    // MARK: Constant Angles
+
+    /// Represents the angle pointing north on a circle in UIKit
     static let circleTopAngle: CGFloat = .pi * 1.5
+
+    /// Represents the angle pointing east on a circle in UIKit
     static let circleRightAngle: CGFloat = 0.0
+
+    /// Represents the angle pointing south on a circle in UIKit
     static let circleBottomAngle: CGFloat = .pi * 0.5
+
+    /// Represents the angle pointing west on a circle in UIKit
     static let circleLeftAngle: CGFloat = .pi
+
+
+    // MARK: Conversion
 
     /// Calculates the degree of an angle corresponding to the given radian
     static func degree (_ rad: CGFloat) -> CGFloat {
