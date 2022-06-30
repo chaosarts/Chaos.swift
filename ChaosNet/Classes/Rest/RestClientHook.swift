6f91c422e9af382d6566de5818d5b91644540c91
Fu Lam Diep <fu.lam@chrono24.com>
2022-06-30 11:45:48 +0200

    Add new Hook Protocols

--- ChaosNet/Classes/Rest/RestClientHook.swift
+++ ChaosNet/Classes/Rest/RestClientHook.swift
@@ -11,6 +11,10 @@ public protocol PreRequestHook {
     func restClient(_ restClient: RestClient, willSendRequest request: RestRequest, relativeTo baseUrl: URL?) async
 }
 
-public protocol RescueRequestHook {
-    func restClient(_ restClient: RestClient, rescueRequest request: RestRequest, relativeTo baseUrl: URL?) async throws -> RestTransportEngineResponse
+public protocol PostRequestHook {
+    func restClient(_ restClient: RestClient, didReceiveResponse response: RestTransportEngineResponse, for request: RestRequest, relativeTo baseUrl: URL?)
+}
+
+public protocol ProcessResponseHook {
+    func restClient<D>(_ restClient: RestClient, didProduceResponse response: RestResponse<D>) -> RestResponse<D>
 }
